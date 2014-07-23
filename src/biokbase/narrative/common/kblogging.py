9e6921532bdb70ce7c9a0704f9cf0c3bae9f36bf
Dan Gunter <dkgunter@lbl.gov>
2014-07-23 12:40:22 -0700

    saving work -- do not run

--- src/biokbase/narrative/common/kblogging.py
+++ src/biokbase/narrative/common/kblogging.py
@@ -6,6 +6,21 @@ __date__ = '1/30/14'
 
 import logging
 import os
+log_to_mongo = False
+try:
+    import pymongo
+    from mongolog.handlers import MongoHandler
+    log_to_mongo = True
+except:
+    pass
+
+class Mongo(object):
+    host = 'localhost'
+    port = 27017
+    user = 'narrative'
+    password = 'letm3in'
+    db = 'logging'
+    collection = 'narrative'
 
 _log = logging.getLogger("biokbase")  # root of hierarchy
 
@@ -15,7 +30,23 @@ if os.environ.get("KBASE_DEBUG", None):
 else:
     _log.setLevel(logging.WARN)
 
-# Set default log format.
+# Set default log handler.
 _h = logging.StreamHandler()
 _h.setFormatter(logging.Formatter("%(levelname)s %(asctime)s %(module)s: %(message)s"))
 _log.addHandler(_h)
+
+# If mongo is available, add that one too
+if log_to_mongo:
+    try:
+        # make sure we can connect
+        client = pymongo.MongoClient(host=Mongo.host, port=Mongo.port)
+        db = client[Mongo.db]
+        db.authenticate(Mongo.user, Mongo.password)
+        del db, client
+        # add handler
+        _log.addHandler(MongoHandler.to(db=Mongo.db, collection=Mongo.collection,
+                                        host=Mongo.host, port=Mongo.port,
+                                        user=Mongo.user, password=Mongo.password))
+    except pymongo.errors.PyMongoError, err:
+        _log.info("Could not connect to to MongoDB for logging: {}".format(err))
+
